b74f8b7b5a16e27c0545652b9f744d64dd872abb
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-10-07 12:02:14 +0430

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import "./globals.css";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>
+
 )

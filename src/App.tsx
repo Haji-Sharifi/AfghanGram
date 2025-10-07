88fa4e6bda8cbe6e1e63375009fb2002077bd638
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-10-07 12:17:53 +0430

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import Home from "./_root/pages/Home";
+
 export default function App() {
   return (
-    <>
-      <h1 className="underline">hello</h1>
-    </>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-in" element={<SignupForm />} />
+
+        {/* Privite routes */}
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </main>
   )
 }
